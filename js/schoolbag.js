a822b193fd740f7299dd165892756b6a13e34d1c
sidimar <sidimar@gmail.com>
2016-05-12 11:14:41 +0100

    Added quotes

--- js/schoolbag.js
+++ js/schoolbag.js
@@ -6,12 +6,12 @@
 
 //Set height for each section
 function pageHeight() {
-    var wHeight = $( window ).height();
-    console.log (wHeight);
+    var wHeight = $(window).height();
+    console.log(wHeight);
     $('header, #howitworks, #benefits, #about, #contatct').css('min-height', wHeight);
 }
 pageHeight()
-$(window).resize(function(){
+$(window).resize(function() {
     pageHeight()
 });
 // jQuery for page scrolling feature - requires jQuery Easing plugin
@@ -35,86 +35,119 @@ $('.navbar-collapse ul li a').click(function() {
     $('.navbar-toggle:visible').click();
 });
 
-// Random image for header section.
+// Loading image for header section randomly
 var images = ['01.jpg', '02.jpg', '03.jpg', '04.jpg', '05.jpg', '06.jpg'];
-$('header').css({'background-image': 'url(img/header/' + images[Math.floor(Math.random() * images.length)] + ')'});
+$('header').css({
+    'background-image': 'url(img/header/' + images[Math.floor(Math.random() * images.length)] + ')'
+});
+
+
+// Loading quotes divs randomly
+var divs = $(".div-quote").get().sort(function() {
+    return Math.round(Math.random()) - 0.5; //random so we get the right +/- combo
+}).slice(0, 2)
+$(divs).show();
 
 
 // Google Maps
 google.maps.event.addDomListener(window, 'load', init);
-    function init() {
-        var mapOptions = {
-            center: new google.maps.LatLng(53.341171,-6.284175),
-            zoom: 18,
-            zoomControl: true,
-            zoomControlOptions: {
-                style: google.maps.ZoomControlStyle.DEFAULT,
-            },
-            disableDoubleClickZoom: true,
-            mapTypeControl: false,
-            scaleControl: false,
-            scrollwheel: false,
-            panControl: true,
-            streetViewControl: true,
-            draggable : true,
-            overviewMapControl: true,
-            overviewMapControlOptions: {
-                opened: false,
-            },
-            mapTypeId: google.maps.MapTypeId.ROADMAP,
+
+function init() {
+    var mapOptions = {
+        center: new google.maps.LatLng(53.341171, -6.284175),
+        zoom: 18,
+        zoomControl: true,
+        zoomControlOptions: {
+            style: google.maps.ZoomControlStyle.DEFAULT,
+        },
+        disableDoubleClickZoom: true,
+        mapTypeControl: false,
+        scaleControl: false,
+        scrollwheel: false,
+        panControl: true,
+        streetViewControl: true,
+        draggable: true,
+        overviewMapControl: true,
+        overviewMapControlOptions: {
+            opened: false,
+        },
+        mapTypeId: google.maps.MapTypeId.ROADMAP,
+    }
+    var mapElement = document.getElementById('map');
+    var map = new google.maps.Map(mapElement, mapOptions);
+    var locations = [
+        ['Learning Data', 'undefined', '(+353) (0)1 410 0604', 'info@learningdata.ie', 'http://learningdata.ie', 53.341171, -6.284175, 'img/solid-pin-orange.png']
+    ];
+    for (i = 0; i < locations.length; i++) {
+        if (locations[i][1] == 'undefined') {
+            description = '';
+        } else {
+            description = locations[i][1];
         }
-        var mapElement = document.getElementById('map');
-        var map = new google.maps.Map(mapElement, mapOptions);
-        var locations = [
-['Learning Data', 'undefined', '(+353) (0)1 410 0604', 'info@learningdata.ie', 'http://learningdata.ie', 53.341171, -6.284175, 'img/solid-pin-orange.png']
-        ];
-        for (i = 0; i < locations.length; i++) {
-            if (locations[i][1] =='undefined'){ description ='';} else { description = locations[i][1];}
-            if (locations[i][2] =='undefined'){ telephone ='';} else { telephone = locations[i][2];}
-            if (locations[i][3] =='undefined'){ email ='';} else { email = locations[i][3];}
-           if (locations[i][4] =='undefined'){ web ='';} else { web = locations[i][4];}
-           if (locations[i][7] =='undefined'){ markericon ='';} else { markericon = locations[i][7];}
-            marker = new google.maps.Marker({
-                icon: markericon,
-                position: new google.maps.LatLng(locations[i][5], locations[i][6]),
-                map: map,
-                title: locations[i][0],
-                desc: description,
-                tel: telephone,
-                email: email,
-                web: web
-            });
-if (web.substring(0, 7) != "http://") {
-link = "http://" + web;
-} else {
-link = web;
-}
-            bindInfoWindow(marker, map, locations[i][0], description, telephone, email, web, link);
-     }
- function bindInfoWindow(marker, map, title, desc, telephone, email, web, link) {
-      var infoWindowVisible = (function () {
-              var currentlyVisible = false;
-              return function (visible) {
-                  if (visible !== undefined) {
-                      currentlyVisible = visible;
-                  }
-                  return currentlyVisible;
-               };
-           }());
-           iw = new google.maps.InfoWindow();
-           google.maps.event.addListener(marker, 'click', function() {
-               if (infoWindowVisible()) {
-                   iw.close();
-                   infoWindowVisible(false);
-               } else {
-                   var html= "<div style='color:#000;background-color:#fff;padding:5px;width:150px;'><h4>"+title+"</h4><p>"+telephone+"<p><a href='mailto:"+email+"' >"+email+"<a><a href='"+link+"'' >"+web+"<a></div>";
-                   iw = new google.maps.InfoWindow({content:html});
-                   iw.open(map,marker);
-                   infoWindowVisible(true);
-               }
+        if (locations[i][2] == 'undefined') {
+            telephone = '';
+        } else {
+            telephone = locations[i][2];
+        }
+        if (locations[i][3] == 'undefined') {
+            email = '';
+        } else {
+            email = locations[i][3];
+        }
+        if (locations[i][4] == 'undefined') {
+            web = '';
+        } else {
+            web = locations[i][4];
+        }
+        if (locations[i][7] == 'undefined') {
+            markericon = '';
+        } else {
+            markericon = locations[i][7];
+        }
+        marker = new google.maps.Marker({
+            icon: markericon,
+            position: new google.maps.LatLng(locations[i][5], locations[i][6]),
+            map: map,
+            title: locations[i][0],
+            desc: description,
+            tel: telephone,
+            email: email,
+            web: web
         });
-        google.maps.event.addListener(iw, 'closeclick', function () {
+        if (web.substring(0, 7) != "http://") {
+            link = "http://" + web;
+        } else {
+            link = web;
+        }
+        bindInfoWindow(marker, map, locations[i][0], description, telephone, email, web, link);
+    }
+
+    function bindInfoWindow(marker, map, title, desc, telephone, email, web, link) {
+        var infoWindowVisible = (function() {
+            var currentlyVisible = false;
+            return function(visible) {
+                if (visible !== undefined) {
+                    currentlyVisible = visible;
+                }
+                return currentlyVisible;
+            };
+        }());
+        iw = new google.maps.InfoWindow();
+        google.maps.event.addListener(marker, 'click', function() {
+            if (infoWindowVisible()) {
+                iw.close();
+                infoWindowVisible(false);
+            } else {
+                var html = "<div style='color:#000;background-color:#fff;padding:5px;width:150px;'><h4>" + title + "</h4><p>" + telephone + "<p><a href='mailto:" + email + "' >" + email + "<a><a href='" + link + "'' >" + web + "<a></div>";
+                iw = new google.maps.InfoWindow({
+                    content: html
+                });
+                iw.open(map, marker);
+                infoWindowVisible(true);
+            }
+        });
+        google.maps.event.addListener(iw, 'closeclick', function() {
             infoWindowVisible(false);
         });
- }
+    }
 }
